4e54b08899c623046b1886a88096ddf01a0ac8d2
divietrodenny-commits <divietrodenny@gmail.com>
2026-02-02 19:43:46 +0100

    Adding user data request

---
+++ JS/main.js
@@ -0,0 +1,6 @@
+/* Richiesta dati utente */
+const km = parseFloat(prompt('Quanti chilometri devi percorrere?'));
+const eta = parseInt(prompt('Qual è l*età del passeggero?'));
+
+console.log(`Kilometri inseriti: ${km}`);
+console.log(`Età inserita: ${eta}`);
