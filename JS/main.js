61c09d13238111e4c023a0f87838f23a8431308b
divietrodenny-commits <divietrodenny@gmail.com>
2026-02-02 19:50:13 +0100

    added calculation of base price, discounts and final price

--- JS/main.js
+++ JS/main.js
@@ -4,3 +4,22 @@ const eta = parseInt(prompt('Qual è l*età del passeggero?'));
 
 console.log(`Kilometri inseriti: ${km}`);
 console.log(`Età inserita: ${eta}`);
+
+/* Calcolo prezzo base*/
+const prezzoBase = km * 0.21;
+
+/* Prezzo finale con sconti */
+let prezzoFinale;
+if (eta < 18) {
+
+ /* Sconto 20% per minorenni */
+ prezzoFinale = prezzoBase * 0.8;
+} else if (eta >= 65) {
+
+/* Sconto 40% per over 65 */
+prezzoFinale = prezzoBase * 0.6;
+} else {
+
+/* Nessun sconto  */
+ prezzoFinale = prezzoBase;
+}
